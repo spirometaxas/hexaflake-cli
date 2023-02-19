68ccaa3b12395db22f9461fe687b30c55f7c6936
Spiro Metaxas <spirometaxas@gmail.com>
2023-02-19 17:44:16 -0500

    Version 1.3.0

--- src/cli.js
+++ src/cli.js
@@ -9,10 +9,10 @@ const printUsage = function(showIntro) {
     console.log('\n' + 
                 ' Usage:\n' + 
                 '   $ hexaflake-cli <n>\n' + 
-                '   $ hexaflake-cli <n> <size>\n' + 
+                '   $ hexaflake-cli <n> [size] [options]\n' + 
                 '\n' + 
                 '   <n> is the recursive step, a number greater than or equal to 0\n' + 
-                '   <size> is the size to draw, a number greater than or equal to <n>\n' + 
+                '   [size] is the size to draw, a number greater than or equal to <n>\n' + 
                 '\n' +
                 ' Options:\n' + 
                 '   --character=<character>  Draw using 1 specific character\n');
